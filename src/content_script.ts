040ca3b8942de05d76edcf52d12cfc07a8eff2b2
doiken <doiken@users.noreply.github.com>
2020-04-07 23:41:14 +0900

    implement popup arrow

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,17 +1,30 @@
 import 'chrome-extension-async';
 
+const classLR = {
+    next: "_QZ_center_right _QZ_circle_right",
+    previous: "_QZ_center_left _QZ_circle_left",
+}
+function insertDom(url: string, prevNext: string) {
+    const hint = document.createElement("link");
+    hint.rel = "prefetch";
+    hint.as = "html";
+    hint.href = url;
+    hint.crossOrigin = "use-credentials";
+    document.head.appendChild(hint);
+
+    const a = document.createElement("a");
+    a.href = url;
+    a.className = `_QZ_center _QZ_circle ${classLR[prevNext]}`;
+    document.body.appendChild(a);
+}
+
 async function appendPrefetchForNeibors() {
     // @ts-ignore
     const neibors = <QiitaApiCache.NeiborMap.Neibors> await chrome.runtime.sendMessage("neibors");
     const keys = Object.keys(neibors);
     if (keys.length === 0) return;
-    keys.forEach(function (key) {
-        const hint = document.createElement("link");
-        hint.rel = "prefetch";
-        hint.as = "html";
-        hint.href = neibors[key];
-        hint.crossOrigin = "use-credentials";
-        document.head.appendChild(hint);
+    keys.forEach((key) => {
+        insertDom(neibors[key], key);
     });
 }
 
